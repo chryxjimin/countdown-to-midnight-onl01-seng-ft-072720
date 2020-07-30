ebbb00a61f169b071d56d5155989caf7a8311282
Christie Yoon <cleex1999@gmail.com>
2020-07-30 22:25:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,14 @@ end
   return "HAPPY NEW YEAR!"
 end
 end
+
+def countdown_with_sleep
+  sleep 1
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+  if number == 0
+  return "HAPPY NEW YEAR!"
+end
+end
